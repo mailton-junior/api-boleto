76cac1e3a5c7e156672f9ba45fe60b20fccf8965
Mailton Junior <mailtonjr7@gmail.com>
2025-01-16 14:06:02 -0300

    TicketController

---
+++ api-boleto/src/main/java/br/com/mailton/api_boleto/controller/TicketController.java
@@ -0,0 +1,28 @@
+package br.com.mailton.api_boleto.controller;
+
+import br.com.mailton.api_boleto.dto.TicketDTO;
+import br.com.mailton.api_boleto.dto.TicketRequestDTO;
+import br.com.mailton.api_boleto.service.TicketService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ticket")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TicketDTO> save(@RequestBody TicketRequestDTO ticketRequestDTO) {
+        TicketDTO ticket = ticketService.save(ticketRequestDTO.getBarcode());
+        return new ResponseEntity<>(ticket, HttpStatus.CREATED);
+    }
+}
---
+++ api-boleto/src/main/java/br/com/mailton/api_boleto/dto/TicketRequestDTO.java
@@ -0,0 +1,15 @@
+package br.com.mailton.api_boleto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketRequestDTO {
+
+    private String barcode;
+}
